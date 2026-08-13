4dcc5bcccf0235a7ce2d190e6fb5b6d1a3e66bb6
Ratio <Vous@exemple.com>
2026-08-13 14:53:54 +0200

    bugfixe ?

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -21197,7 +21197,7 @@ export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
 	sixseven: {
 		num: 10067,
 		name: "67",
-		types: ["Steel","Stellar"],
+		types: ["Steel"],
 		gender: "N",
 		baseStats: { hp: 67, atk: 67, def: 67, spa: 67, spd: 67, spe: 67},
 		abilities: { 0: "6767" },
